5917c00baa0c57ab1c73be13602d959d314285ae
swm_231 <1056338797@qq.com>
2023-04-27 20:00:44 +0800

    add biew

--- init_before_use1.cpp
+++
@@ -1,9 +0,0 @@
-#include "foo.h"
-extern Foo& globalFoo();
-class Bar{
-public:
-    explicit Bar(const Foo& foo){
-        printf("Bar(const Foo&) foo.a = %d\n", foo.a_);
-    }
-};
-Bar bar(globalFoo());
